8898a19138779a4bfcb1b1fde8de8700aadc8a31
Marcos.Santos <Marcos.Santos@users.noreply.github.com>
2021-02-11 18:40:55 -0300

    Update units.java

--- Desafio/Desafio/src/main/java/com/Justa/Desafio/units.java
+++ Desafio/Desafio/src/main/java/com/Justa/Desafio/units.java
@@ -1,5 +1,23 @@
 package com.Justa.Desafio;
 
 public class units {
+	
+	private String unit_name;
+	private Number mutiplication_factor;
+	
+	public units(String unit_name,Number mutiplication_factor) {
+		
+		this.unit_name = unit_name;
+		this.mutiplication_factor = mutiplication_factor;
+	}
+	
+	public String getUnit_name() {
+		return unit_name;
+	}
 
+	public Number getMutiplication_factor() {
+		return mutiplication_factor;
+	}
+
+	
 }
